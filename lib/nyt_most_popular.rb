7f96466b712664b4d3b63b5208d07d1476f90312
allison cortez <allizetorc@gmail.com>
2019-11-23 21:09:54 +0000

    updates to run file

--- lib/nyt_most_popular.rb
+++ lib/nyt_most_popular.rb
@@ -1,7 +1,10 @@
-require "nyt_most_popular/version"
-
 #where we need to require all gems
-
 #need to know:require vs require relative
 
+
 require 'pry'
+
+require "nyt_most_popular/cli"
+require "nyt_most_popular/api"
+require "nyt_most_popular/articles"
+require "nyt_most_popular/version"
